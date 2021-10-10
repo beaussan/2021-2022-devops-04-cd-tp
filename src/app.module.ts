74bd1bb51707e10c54ab40c25aa50c65099e1f24
Nicolas Beaussart <nic.beaussart@gmail.com>
2021-10-10 12:08:16 +0200

    Add db + config manager

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from './config/config.module';
+import { ConfigService } from './config/config.service';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (
+        configService: ConfigService,
+      ): Promise<TypeOrmModuleOptions> => ({
+        type: 'postgres',
+        url: configService.databaseUrl,
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+        logging: configService.isLoggingDb ? 'all' : false,
+      }),
+      inject: [ConfigService],
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/config.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@nestjs/common';
+import * as z from 'zod';
+import { config as parseConfig } from 'dotenv';
+
+const schema = z.object({
+    NODE_ENV: z.enum(["development", "production", "test"]).default('development'),
+    LOG_LEVEL: z.enum(['error', 'warning', 'info', 'debug', 'silly']).default('debug'),
+    DATABASE_URL: z.string().nonempty(),
+    LOG_SQL_REQUEST: z.string().transform(value => value === 'true').default('false'),
+})
+
+type EnvConfig = z.infer<typeof schema>
+
+@Injectable()
+export class ConfigService {
+  private readonly envConfig: EnvConfig;
+
+  constructor() {
+    try {
+      parseConfig();
+    } catch (e) {}
+
+    this.envConfig = this.validateInput(process.env);
+  }
+
+  /**
+   * Ensures all needed variables are set, and returns the validated JavaScript object
+   * including the applied default values.
+   */
+  private validateInput(envConfig: any): EnvConfig {
+
+    const result = schema.safeParse(envConfig);
+
+    if (result.success === false) {
+      throw new Error(`Config validation error: ${result.error.toString()}`);
+    }
+    return result.data;
+  }
+
+  get nodeEnv(): string {
+    return this.envConfig.NODE_ENV;
+  }
+
+  get databaseUrl(): string {
+    return this.envConfig.DATABASE_URL;
+  }
+
+  get isLoggingDb(): boolean {
+    return this.envConfig.LOG_SQL_REQUEST;
+  }
+
+  get loggerLevel(): string {
+    return this.envConfig.LOG_LEVEL;
+  }
+
+}
